f3ade6a4423c71bdf3518429ce7e842113e0a47b
KylieHoltorf <kylie.holtorf.03@gmail.com>
2025-02-18 20:49:34 +0000

    Not done yet

--- CaffeinatedBeverage.java
+++ CaffeinatedBeverage.java
@@ -1,8 +1,32 @@
-public class CaffeinatedBeverage
-{
+public class CaffeinatedBeverage {
+
+    /*************CONSTANTS*************/
+
+    public static final String defaultName = "Yerba";
+    public static final int defaultOunces = 0;
+    public static final double defaultPrice = 0.0;
+
+    /*************INSTANCE VARIABLES*************/
+
     private String name;
     private int ounces;
     private double price;
+
+    /*************CONSTRUCTORS*************/
+    /**
+     * Default Constructor 
+     */
+
+    public CaffeinatedBeverage() {
+        this(CaffeinatedBeverage.defaultName, CaffeinatedBeverage.defaultOunces, CaffeinatedBeverage.defaultPrice);
+    }
+
+    /**
+     * Full Constructor
+     * @param name
+     * @param ounces
+     * @param price
+     */
 
     public CaffeinatedBeverage(String name, int ounces, double price) {
         this.name = name;
@@ -10,36 +34,69 @@ public class CaffeinatedBeverage
         this.price = price;
     }
 
+    /* 
+    public CaffeinatedBeverage(CaffeinatedBeverage original) {
+        if (original != null) {
+            
+        }
+    }
+    */
+    
+    /*************GETTERS*************/
+
     public String getName() {
         return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getOunces() {
         return this.ounces;
     }
 
-    public void setOunces(int ounces) {
-        this.ounces = ounces;
-    }
-
     public double getPrice() {
         return this.price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    /*************SETTERS*************/
+
+    public void setName(String name) {
+        this.name = name;
     }
+
+    public boolean setOunces(int ounces) {
+        if (ounces >= 0) {
+            this.ounces = ounces;
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public  boolean setPrice(double price) {
+        
+
+        if (price >= 0) {
+            this.price = price;
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    /*************OTHER METHODS*************/
 
     @Override
     public boolean equals(Object o) {
-        if (o == null || this.getClass() != o.getClass()) return false;
+        if (o == null || this.getClass() != o.getClass())
+            return false;
         CaffeinatedBeverage that = (CaffeinatedBeverage) o;
         return this.ounces == that.ounces &&
                 Double.compare(this.price, that.price) == 0 &&
-               this.name.equals(that.name);
+                this.name.equals(that.name);
+    }
+
+    public boolean sip(int ounces) {
+        
     }
 }
